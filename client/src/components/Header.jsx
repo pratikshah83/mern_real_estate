3ab68d428f50b85fbf2bb2cf51a12810186598fb
Pratik <pratikshah83@gmail.com>
2023-10-24 20:57:34 +0530

    create middleware and function to handle errors

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -8,14 +8,14 @@ export default function Header() {
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to="/">
           <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-            <span className="text-slate-500">Pratik's</span>
+            <span className="text-slate-500">Prisha's</span>
             <span className="text-slate-700">Estate</span>
           </h1>
         </Link>
         <form className="bg-slate-300 p-3 rounded-lg flex items-center">
           <input
             className="bg-transparent focus:outline-none w-24 sm:w-64"
-            placeholder="Search..."
+            placeholder="Prisha..."
             type="text"
           ></input>
           <FaSearch className="text-slate-600"></FaSearch>
